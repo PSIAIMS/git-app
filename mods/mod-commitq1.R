720c70c5eaed50fe162354715f780047350379c8
statasarus <christinaefillmore@gmail.com>
2023-10-09 12:19:20 +0100

    Modify commit q1

--- mods/mod-commitq1.R
+++ mods/mod-commitq1.R
@@ -1,7 +1,9 @@
 commitq1_ui <- function(id){
   ns <- NS(id)
   tagList(
-    h3("Make a commit history that matches the following diagram:"),
+    h3("Practice making commits:"),
+    p('Try to copy the commit network graph below, 
+      by checking each commit message and creating a similar commit below.'),
     div(
       div(
         class = "dot",
@@ -34,6 +36,9 @@ commitq1_server <- function(id){
     function(input, output, session){
       ns <- session$ns
       
+      num_dots <- reactiveVal(0)
+      messages <- reactiveVal(NULL)
+      
       observeEvent(input$commit_btn, {
         if (input$input_text != "") {
           insertUI(
@@ -45,17 +50,69 @@ commitq1_server <- function(id){
             )
           )
           updateTextInput(session, "input_text", value = "")
+          disable("commit_btn")
+          enable("undo_btn")
+          num_dots(num_dots()+1)
+          messages(c(messages(), input$input_text))
         }
+        
+      })
+      
+      observeEvent(input$input_text, {
+        if (input$input_text == "") {
+          disable("commit_btn")
+        } else if(num_dots() < 3) {
+          enable("commit_btn")
+        }
+        
       })
       
       observeEvent(input$undo_btn, {
-        removeUI(selector = paste0("#",  ns("commit_ls")))
-        insertUI(
-          selector = paste0("#",  ns("answers")),
-          where = "beforeEnd",
-          ui = div(id = ns("commit_ls"), class = "branch")
-        )
+        runjs(str_glue(
+          "$('#{ns('commit_ls')}').find('.dot:last').remove();"
+        ))
         
+        messages(messages()[-length(messages())])
+        
+        num_dots(num_dots()-1)
+      })
+      
+      observe({
+        if(num_dots() < 1){
+          disable("undo_btn")
+        } else if(num_dots() == 3){
+          disable("commit_btn")
+          test <- setequal(messages(), c("My first commit",
+                                "Next commit",
+                                "Final commit"))
+          if(test){
+            showModal(
+              modalDialog(
+                title = "Congratulations",
+                p("You have matched the network graph right down to the commit messages! Time for the next challenge!"),
+                footer=tagList(
+                  actionButton(ns('close'), 'close')
+                )
+              )
+            )
+          } else {
+            showModal(
+              modalDialog(
+                title = "Oops",
+                p("You matched the general shape of the network, but your commit messages are different. 
+                  Use the 'undo' button to back and hover over each commit above to check the messages."),
+                footer=tagList(
+                  actionButton(ns('close'), 'close')
+                )
+              )
+            )
+          }
+        }
+        
+      })
+      
+      observeEvent(input$close,{
+        removeModal()
       })
       
     }
